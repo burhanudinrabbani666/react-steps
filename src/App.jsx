59b62bd61914bab92a9511034399ca2eb373f5ab
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-10 13:49:27 +0700

    feat: Handling Evenet in React jsx with onclick.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ const messages = [
 export default function App() {
   const step = 1;
 
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {}
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -20,10 +26,16 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          onClick={handlePrevious}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          onClick={handleNext}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
           Next
         </button>
       </div>

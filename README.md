e62314531e1e36e1be7cabcc47b0b1e3f3741f67
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-10 14:57:31 +0700

    feat:  theory again about useState and implement toogle open

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This repository is part of my react learning repository react: <br>
 | 4   | [What is react State](./notes/4.md)                      |        |       |
 | 5   | [Creating a state variable](./notes/5.md)                |        |       |
 | 6   | [Dont state manually](./notes/6.md)                      |        |       |
-| 7   | [The Mechanics of states](./notes/7.md)                  |        |       |
-| 8   | [Adding Another piece of state](./notes/8.md)            |        |       |
+| 7   | [The Mechanics of states](./notes/7&8.md)                |        |       |
+| 8   | [Adding Another piece of state](./notes/7&8.md)          |        |       |
 | 9   | [React Developers tools](./notes/9.md)                   |        |       |
 | 10  | [Updateing state Based on Current State](./notes/10.md)  |        |       |
 | 11  | [More thoughts About State + state Guide](./notes/11.md) |        |       |
--- src/App.jsx
+++ src/App.jsx
@@ -8,42 +8,70 @@ const messages = [
 
 export default function App() {
   // useState in Practice
+  // Dont use Let
+  // const [test, setTest] = useState({ name: "Jonas" });
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
   }
 
   function handleNext() {
+    // step = step + 1; // ❗ Dont set State Manually
+    // test.name = "fred"; // ❗ BAD Practice
+    // setTest({ name: "Bani" });
     if (step < 3) setStep(step + 1);
   }
 
+  function handleOpen() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    //REACT FRAGMENT
+    <>
+      <button
+        onClick={handleOpen}
+        className="close" //
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
 
-      <div className="buttons">
-        <button
-          onClick={handlePrevious}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          Previous
-        </button>
-        <button
-          onClick={handleNext}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button
+              onClick={handlePrevious}
+              style={{
+                backgroundColor: "#7950f2",
+                color: "#fff",
+              }}
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              style={{
+                backgroundColor: "#7950f2",
+                color: "#fff",
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
